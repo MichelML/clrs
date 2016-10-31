97bac7541ed8bf27407661d2781f44458d3d46f3
MichelML <michmoreau.l@gmail.com>
2016-10-31 12:01:43 -0400

    feat: add insert_sort js implementation

---
+++ js/insert_sort.js
@@ -0,0 +1,42 @@
+/**
+ * Sorting problem:
+ *
+ * Input: A sequence of n numbers {a1, a2, ... , an }
+ *
+ * Output: A permutation (reordering) {a1', a2', ... , an' } 
+ * */
+function insertSort(array)
+{
+
+    var startIndex = 1;
+    var key;
+    var indexOfValueToInsert;
+
+    for (; startIndex < array.length; startIndex++)
+    {
+
+        key = array[startIndex];
+        indexOfValueToInsert = startIndex - 1;
+
+        while (indexOfValueToInsert > -1 && array[indexOfValueToInsert] >
+            key)
+        {
+
+            array[indexOfValueToInsert + 1] = array[indexOfValueToInsert];
+            indexOfValueToInsert--;
+
+        }
+
+        array[indexOfValueToInsert + 1] = key;
+
+    }
+
+    console.log(array);
+
+}
+
+insertSort([2, 1, 6, 7, 8, 1, 2, 3, 4, 5]);
+insertSort([203894750923874098, 023452345234, 090809809809808, 6464646466, 999999,234,23,3,-19819891898, -19]);
+insertSort([90,80,70,60,40,20,10]);
+insertSort([1000,900,800,400,600,700,200,100,30,50,60,70,801,10000,1,23452345, -34, -1, -1000, -13456345]);
+insertSort([-10, 0 , 0 , 0 , 0 , 0 ,0 , -20, -20.1, 10.2, 1000.50, 90.112341234123]);
