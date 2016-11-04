dbdc917202299ece996fb74ff7e990cb79cff95c
MichelML <michmoreau.l@gmail.com>
2016-11-04 13:10:13 -0400

    feat: add similar testing to insertion sort compared to merge sort

--- js/insertion_sort.js
+++ js/insertion_sort.js
@@ -33,8 +33,24 @@ const insertionSort = (array) => {
 
 };
 
-console.log(insertionSort([2, 1, 6, 7, 8, 1, 2, 3, 4, 5]));
-console.log(insertionSort([203894750923874098, 23452345234, 90809809809808, 6464646466, 999999, 234, 23, 3, -19819891898, -19]));
-console.log(insertionSort([90, 80, 70, 60, 40, 20, 10]));
-console.log(insertionSort([1000, 900, 800, 400, 600, 700, 200, 100, 30, 50, 60, 70, 801, 10000, 1, 23452345, -34, -1, -1000, -13456345]));
-console.log(insertionSort([-10, 0, 0, 0, 0, 0, 0, -20, -20.1, 10.2, 1000.50, 90.112341234123]));
+console.log(insertionSort(generateArr(1, 100)));
+console.log(insertionSort(generateArr(100, -1000)));
+console.log(insertionSort(generateArr(1001, 100)));
+console.log(insertionSort(generateArr(105, 100000)));
+console.log(insertionSort(generateArr(10000, 100000000)));
+console.log(insertionSort(generateArr(10000, 100000000, true)));
+
+function generateArr(length, range, withdecimals) {
+    let arr = [];
+
+    while (arr.length < length) {
+
+        let randomnumber = Math.ceil(Math.random() * range) + ((withdecimals) ? Math.random() : 0);
+        arr[arr.length] = randomnumber;
+
+    }
+
+    return arr;
+
+}
+
