8c3513060a3fb9130df32db31b680ab2744732ed
Michel Moreau <michel.moreau.lapointe@gmail.com>
2016-11-06 09:45:44 -0500

    fix: remove commented code for unfinished implementation

--- clojure/insertion_sort.clj
+++ clojure/insertion_sort.clj
@@ -11,17 +11,3 @@
 ;; we need a function that determines what is the right index of a value
 
 ;; NOT COMPLETED
-"(defn insertion_sort [array] 
-  ((def arr array)
-  (def i 0)
-  (def j 1)
-  (for [value (rest array)] 
-    ((alter-var-root #'j ((constantly (+ j 1))))
-    (alter-var-root #'i ((constantly (- j 1))))
-    (while (and (> i 0) (> (get array i))) 
-      (alter-var-root #'arr ((constantly (assoc arr (+ i 1) (get array i)))))
-      (alter-var-root #'i ((constantly (- i 1)))))
-    (alter-var-root #'arr ((constantly ((assoc arr (+ i 1) value)))))))
-   (print arr)))
-
-(insertion_sort (vector 5 4 3 2 1))"
