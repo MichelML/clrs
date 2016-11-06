975fccf01949e33f4c41b53a3509731f987c8497
Michel Moreau <michel.moreau.lapointe@gmail.com>
2016-11-06 09:49:26 -0500

    refactor: remove old comments

--- clojure/insertion_sort.clj
+++ clojure/insertion_sort.clj
@@ -4,10 +4,4 @@
 ;;
 ;;Output: A permutation (reordering) {a1', a2', ... , an' } 
 
-;;we need a function to push the current value at the right index
-
-;;we need a function that gets the value in the array to repush at the right index
-
-;; we need a function that determines what is the right index of a value
-
 ;; NOT COMPLETED
